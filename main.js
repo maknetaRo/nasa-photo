5b124e8fda8346cf95cbd86d257acd7969d5a0cd
maknetaRo <makneta@gmail.com>
2020-07-26 13:39:03 +0200

    add iframe

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const cross = document.querySelector(".cross");
 const text = document.querySelector(".text");
 const title = document.querySelector(".title");
 const dateDiv = document.querySelector(".date");
+const videoContainer = document.querySelector(".video-container");
 
 btn.addEventListener("click", () => {
   btn.style.display = "none";
@@ -28,13 +29,36 @@ function getResult() {
     })
     .then(displayResult);
 }
+function displayVideo(result) {
+  let video = document.createElement("iframe");
+  video.setAttribute("src", "movie.mp4");
+  video.setAttribute("width", "100%");
+  video.setAttribute("height", "100%");
+  video.setAttribute("position", "absolute");
+  video.setAttribute("controls", "controls");
+  video.classList.add("video");
+  video.src = `${result.url}`;
+}
 
 function displayResult(result) {
+  if (`${result.media_type}` !== "video") {
+    document.body.style.backgroundImage = `url(${result.url})`;
+  } else if (`${result.media_type}` === "video") {
+    displayVideo(result);
+  }
+
   btn.innerHTML = result.title;
-  document.body.style.backgroundImage = `url(${result.url})`;
   text.innerHTML = result.explanation;
   title.innerHTML = result.title;
 }
+
+/*
+video.addEventListener("onended", () => {
+  console.log("Does it work?");
+  this.load();
+  this.play();
+});
+*/
 
 const setTime = setInterval(() => {
   let now = moment();
